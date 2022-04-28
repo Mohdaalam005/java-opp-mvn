b8864ec24838682cefd153b7d30b1765e28dbd45
idrisi aalam <mohdaalam005@gmail.com>
2022-04-28 11:44:27 +0530

    working with list

---
+++ src/com/example/list/DemoLIst.java
@@ -0,0 +1,19 @@
+package com.example.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DemoLIst {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>( );
+        list.add(6);
+        list.add(5);
+        list.add(4);
+        list.add(2);
+        list.add(2);
+
+        System.out.println(list);
+
+    }
+
+}
